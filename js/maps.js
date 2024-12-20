9764701bc930cdff68e7d85572943d371cb02bb4
YuriBPS <veramoralesyuri@gmail.com>
2024-12-20 03:24:08 -0500

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -4,45 +4,59 @@ var map = L.map("map").setView([-10.87415,-74.94391],8);
 //Agregar mapa base del osm
 var osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-//Enlazar servicios wms
-
-var secto1 = L.tileLayer.wms("http://localhost:8080/geoserver/ET_CCUSAF/wms?",{
-					layers		:"PG_SECTOR1_ATFFS_SC",
-					format		:"image/png",
-					Transparent	: true
+// Crear las capas GeoJSON vacías
+var secto1 = L.geoJSON(null, {
+    style: {
+        color: "#ff7800",
+        weight: 2,
+        opacity: 0.65
+    }
 }).addTo(map);
 
-
-var CUSAF = L.tileLayer.wms("http://localhost:8080/geoserver/ET_CCUSAF/wms?",{
-					layers		:"PG_CUSAF_AP_ATFFS_SC",
-					format		:"image/png",
-					Transparent	: true
-
+var CUSAF = L.geoJSON(null, {
+    style: {
+        color: "#0078ff",
+        weight: 2,
+        opacity: 0.65
+    }
 }).addTo(map);
 
-
-var AREAINICIO = L.tileLayer.wms("http://localhost:8080/geoserver/ET_CCUSAF/wms?",{
-					layers		:"AREA_INICIO",
-					format		:"image/png",
-					Transparent	: true
-
+var AREAINICIO = L.geoJSON(null, {
+    style: {
+        color: "#00ff78",
+        weight: 2,
+        opacity: 0.65
+    }
 }).addTo(map);
 
+// Cargar los datos GeoJSON
+fetch('data/secto1.geojson')
+    .then(response => response.json())
+    .then(data => {
+        secto1.addData(data);
+    });
 
+fetch('data/cusaf.geojson')
+    .then(response => response.json())
+    .then(data => {
+        CUSAF.addData(data);
+    });
+
+fetch('data/areainicio.geojson')
+    .then(response => response.json())
+    .then(data => {
+        AREAINICIO.addData(data);
+    });
 
 //agregar controlador de capas
-
 var baseMaps = {
-				"openstreetmap": osm
+    "openstreetmap": osm
 };
 
-var wms = {
-			"Areas Potenciales CUSAF": CUSAF,
-			"Áreas de Inicio Aprobados": secto1,
-			"Área de Inicio SM" : AREAINICIO
-
+var overlayMaps = {
+    "Areas Potenciales CUSAF": CUSAF,
+    "Áreas de Inicio Aprobados": secto1,
+    "Área de Inicio SM": AREAINICIO
 };
 
-L.control.layers(baseMaps,wms).addTo(map);
-
-
+L.control.layers(baseMaps, overlayMaps).addTo(map);
